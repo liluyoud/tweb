cf7dcb4b6735a27a5b55203f0c2cafa69997ea98
Liluyoud Cury de Lacerda <liluyoud@outlook.com>
2018-09-22 09:13:08 -0400

    Adicionar arquivos de projeto.

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class HomeController: Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new IndexViewModel();
+            model.Titulo = "Meu modelo";
+            model.Hoje = DateTime.Now;
+            model.Soma = 10;
+            return View(model);
+        }
+
+        public IActionResult Ola()
+        {
+            return View();
+        } 
+
+        public IActionResult Soma()
+        {
+            var obj = new { Id = 1, Nome = "lilo" };
+            return Ok(obj);
+        }
+    }
+}
---
+++ WebApp/Models/IndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class IndexViewModel
+    {
+        public string Titulo { get; set; }
+        public DateTime Hoje { get; set; }
+        public int Soma { get; set; }
+    }
+}
