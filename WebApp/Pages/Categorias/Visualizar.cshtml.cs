31b718f9ae8abd37a8a2f05acf52fd4fb6342c20
Liluyoud Cury de Lacerda <liluyoud@outlook.com>
2018-09-22 18:28:37 -0400

    Pages

---
+++ WebApp/Pages/Categorias/Visualizar.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApp.Entities;
+
+namespace WebApp.Pages.Categorias
+{
+    public class VisualizarModel : PageModel
+    {
+        private WebAppContext db;
+
+        public VisualizarModel(WebAppContext db)
+        {
+            this.db = db;
+        }
+       
+        public int Id { get; set; }
+
+        public string Descricao { get; set; }
+
+        public void OnGet(int id)
+        {
+            var categoria = db.Categorias.SingleOrDefault(p => p.Id == id);
+            Id = categoria.Id;
+            Descricao = categoria.Descricao;
+        }
+    }
+}
